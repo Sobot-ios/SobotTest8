49392d3fa2380ff8daa3c054516551cd361a162f
lizhihui <lizh@sobot.com>
2015-12-24 15:30:05 +0800

    0.0.1

---
+++ include/SobotTest8/SobotTest8.h
@@ -0,0 +1,13 @@
+//
+//  SobotTest8.h
+//  SobotTest8
+//
+//  Created by lizhihui on 15/12/24.
+//  Copyright © 2015年 lizhihui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SobotTest8 : NSObject
+- ( void)AA:(NSString*)str;
+@end
